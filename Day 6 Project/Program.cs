3516fd737660b117739436bf292944194c6c8f09
Elsayed Rezkallah <elsayedrezkallah1@gmail.com>
2024-07-11 14:19:42 +0300

    Add project files.

---
+++ Day 6 Project/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Day_6_Project
+{
+    internal class Program
+    {
+        #region Task 3
+        //class Employee
+        //{
+
+        //    int Age;
+        //    string Name;
+        //    string Phone;
+        //    double SSN;
+
+        //    public void getData(int age, string name, string phone, double ssn) 
+        //    {
+        //        Age = age;
+        //        Name = name;
+        //        Phone = phone;
+        //        SSN = ssn;
+        //    }
+        //    public void print()
+        //    {
+        //        Console.WriteLine($"Name: {Name} \nPhone: {Phone} \nAge: {Age} \nSSN: {SSN}");
+        //    }
+        //}
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //Console.WriteLine("Please Enter the Number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //for(int i = 0; i <= num; i++) 
+            //{
+            //    Console.WriteLine($"{num} * {i} = {num * i} ");
+            //}
+            #endregion
+
+            #region Task 2
+            //Console.Write("Please Enter your ID: ");
+            //double ID = double.Parse(Console.ReadLine());
+
+            //Console.Write("Pleae Enter your name: ");
+            //string name = Console.ReadLine();
+
+            //Console.Write("Please Enter Your age: ");
+            //double age = double.Parse(Console.ReadLine());
+
+            //do
+            //{
+            //    Console.WriteLine("That is Invalid Age... ");
+            //    Console.Write("Please Enter a Valid Age: ");
+            //    age = double.Parse(Console.ReadLine());
+
+            //} while (age <= 0);
+
+            //Console.WriteLine("Ok, That's it....");
+
+            //Console.ReadLine();
+            #endregion
+
+            #region Task 3
+            //Employee employee1 = new Employee();
+            //employee1.getData(20, "Elsayed Rezkallah", "01060466759", 30313120300);
+            //employee1.print();
+            #endregion
+        }
+    }
+}
